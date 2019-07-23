3848dacd343c9e9bc1806fd58e6b8b22f99ecff3
Jyotishmoy Borah <jyotishmoy98@gmail.com>
2019-07-23 18:20:20 +0530

    Updates

---
+++ precache-manifest.863d41b5842d2319c44790d6740e3d0d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "306140837f0b28b7efc803602179a8a5",
+    "url": "/react-redux-songs/index.html"
+  },
+  {
+    "revision": "72a1de2f1cf5537ea454",
+    "url": "/react-redux-songs/static/js/2.0f20f19b.chunk.js"
+  },
+  {
+    "revision": "69a9795165fcef6ff0d2",
+    "url": "/react-redux-songs/static/js/main.ad8422a3.chunk.js"
+  },
+  {
+    "revision": "d18942d1dea973bf867f",
+    "url": "/react-redux-songs/static/js/runtime~main.7647d0c7.js"
+  }
+]);
